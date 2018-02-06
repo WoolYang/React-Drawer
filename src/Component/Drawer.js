65f58b3f6f32d3c4acd8e391dfd1889d0ac44def
woolyang <249877448@qq.com>
2018-02-06 14:17:30 +0800

    结构优化

--- src/Component/Drawer.js
+++ src/Component/Drawer.js
@@ -1,57 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import initReactFastclick from 'react-fastclick';
 import '../style/drawer.less';
-
-class Drawer extends React.Component {
+initReactFastclick();
+export default class Drawer extends React.Component {
 
     componentDidMount() {
-        if (!!this.props.visible) {
-            document.body.classList.add('drawer-open');
-        }
-
-        /*        let mql = window.matchMedia('(orientation: portrait)');
-                this.handleOrientationChange(mql);
-                mql.addListener(this.handleOrientationChange);*/
-    }
-
-    /*    componentWillUnmount() {
-            let mql = window.matchMedia('(orientation: portrait)');
-            mql.removeListener(this.handleOrientationChange);
-        }*/
-
-    //监听横屏变化
-    handleOrientationChange(mql) {
-        if (mql.matches) {
-            document.querySelector('html').style.fontSize = 'calc(100vw/3.75)'; // 竖屏
-        } else {
-            document.querySelector('html').style.fontSize = 'calc(100vw/6.8)'; // 横屏
-        }
+        const { visible } = this.props
+        visible && document.body.classList.add('drawer-open');
     }
 
     // 关闭浮层
     handleModalClicked(tag, value) {
+        const { changeVisible } = this.props
         document.body.classList.remove('drawer-open');
-        this.props.changeVisible(tag, value);
+        changeVisible && changeVisible(tag, value);
     }
 
     render() {
-        let {
-            style,
-            visible,
-            maskStyle,
-            maskAnimation,
-            animation,
-            children
-        } = this.props;
-        let wrap_className = classnames('drawer-wrap', //蒙层动画
-            visible ? 'drawer-show' : 'drawer-hide', !!visible && maskAnimation ? 'drawer-mask-fadeIn' : '');
+        const { style, visible, maskStyle, maskAnimation, animation, children } = this.props;
 
-        let ctn_className = classnames('drawer-content ', !!visible ? `drawer-ctn-${animation}` : '');
+        const wrap_className = classnames('drawer-wrap', {
+            ['drawer-show']: visible,
+            ['drawer-hide']: !visible,
+            ['drawer-mask-fadeIn']: visible && maskAnimation
+        });
+
+        const ctn_className = classnames('drawer-content ', {
+            [`drawer-ctn-${animation}`]: visible
+        });
 
 
         return (
-            <div className={wrap_className} style={maskStyle}  onClick={this.handleModalClicked.bind(this)}>         
-                <div className={ctn_className} style={!!visible ? style : {width:'0%'}} onClick={e => e.stopPropagation()}>
+            <div className={wrap_className} style={maskStyle} onClick={this.handleModalClicked.bind(this)}>
+                <div className={ctn_className} style={visible ? style : { width: '0%' }} onClick={e => e.stopPropagation()}>
                     {children}
                 </div>
             </div>
@@ -60,11 +43,16 @@ class Drawer extends React.Component {
 }
 
 Drawer.defaultProps = {
-    style: {}, //窗口样式
     visible: false, //是否关闭窗口
-    maskStyle: {}, //蒙层样式
     maskAnimation: false,
-    animation: 'none', //slide fade none
-    changeVisible: function() {} //关闭事件
+    animation: 'none' //slide fade none
 }
-module.exports = Drawer;
+
+Drawer.propTypes = {
+    style: PropTypes.object,
+    visible: PropTypes.bool,
+    maskStyle: PropTypes.object,
+    maskAnimation: PropTypes.bool,
+    animation: PropTypes.oneOf(['slide', 'fade', 'none']),
+    changeVisible: PropTypes.func
+}
